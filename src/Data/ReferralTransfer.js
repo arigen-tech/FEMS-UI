9e190a3809aea9b4a0b4727c172cb945c3de422e
aanandkumar21 <76147622+aanandkumar21@users.noreply.github.com>
2026-08-21 11:34:14 +0530

    UI-updated for Referral Transfer page

--- src/Data/ReferralTransfer.js
+++ src/Data/ReferralTransfer.js
@@ -1,86 +1,15 @@
-import React from 'react';
-import AutoTranslate from '../i18n/AutoTranslate'; // Import AutoTranslate
+import React, { useState } from 'react';
+import ReferralTransferComponent from './ReferralTransferComponent';
+import ReferralTransferView from './ReferralTransferView';
 
 const ReferralTransfer = () => {
-  return (
-    <div className="">
-      <div className="title">
-        <h1><AutoTranslate>Referral / Transfer</AutoTranslate></h1>
-      </div>
-
-      <div class="card">
-
-            <div class="grid grid-col-4 mb-4">
-                <div class="form-group"><label for="itemsPerPage"><span class="">Show:</span></label><select id="itemsPerPage">
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="15">15</option>
-                    <option value="20">20</option>
-                </select></div>
-                <div class="form-group"><label for="searchId"><span class="">Search</span></label>
-                    <input type="text" id="searchId" placeholder="Search..." class="searchIcon" value="" />
-                </div>
-            </div>
-
-            <div class="table-wrapper">
-                <table class="">
-                    <thead>
-                        <tr>
-                            <th><AutoTranslate>Case No.</AutoTranslate></th>
-                            <th><AutoTranslate>From</AutoTranslate></th>
-                            <th><AutoTranslate>To</AutoTranslate></th>
-                            <th><AutoTranslate>Evidence</AutoTranslate></th>
-                            <th><AutoTranslate>Date</AutoTranslate></th>
-                            <th><AutoTranslate>Status</AutoTranslate></th>
-                         </tr>
-                    </thead>
-
-                    <tbody>
-                        <tr>
-                            <td><AutoTranslate>DFSL/145/2026</AutoTranslate></td>
-                            <td><AutoTranslate>DFSL Khordha</AutoTranslate></td>
-                            <td><AutoTranslate>SFSL Bhubaneswar</AutoTranslate></td>
-                            <td><AutoTranslate>DNA</AutoTranslate></td>
-                            <td><AutoTranslate>18-Aug</AutoTranslate></td>
-                            <td><span className='pending'>Pending</span></td>                         
-                        </tr>
-                        {/* <tr>
-                            <td colspan="10" class="text-center"><span class="">No data found.</span></td>
-                        </tr> */}
-                    </tbody>
-                </table>
-            </div>
- 
-            {/* Pagination Controls */}
-            <div class="paginationWp">
-                <div class="items">
-                    <div class="paginationText">
-                        <span class="text-sm text-gray-700">
-                            <span class="">Showing 0 to 0 of 0 entries.</span></span>
-                        <span class="text-sm text-gray-700 mx-2">(<span class="">Pages</span> 0)</span>
-                    </div>
-                </div>
-                <div class="items">
-                    <div class="paginationBtn"><button title="End" disabled="" class="cursor-not-allowed"><svg
-                        stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em"
-                        width="1em" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M217.9 256L345 129c9.4-9.4 9.4-24.6 0-33.9-9.4-9.4-24.6-9.3-34 0L167 239c-9.1 9.1-9.3 23.7-.7 33.1L310.9 417c4.7 4.7 10.9 7 17 7s12.3-2.3 17-7c9.4-9.4 9.4-24.6 0-33.9L217.9 256z">
-                        </path>
-                    </svg></button><button title="End" disabled="" class="cursor-not-allowed"><svg stroke="currentColor"
-                        fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z">
-                        </path>
-                    </svg></button></div>
-                </div>
-            </div>
-
-        </div>     
-
-    </div>
-  )
+    const [transferView, setTransferView] = useState(false);
+    return (
+        <>
+            {!transferView && <ReferralTransferComponent onView={() => setTransferView(true)} />}
+            {transferView && <ReferralTransferView onBack={() => setTransferView(false)} />}
+        </>
+    )
 }
 
 export default ReferralTransfer;
--- src/Data/ReferralTransferView.js
+++ src/Data/ReferralTransferView.js
@@ -2,15 +2,135 @@ import React, { useState } from 'react';
 import AutoTranslate from '../i18n/AutoTranslate'; // Import AutoTranslate
 import { MdRemoveRedEye, MdOutlineClose } from "react-icons/md";
 
-const FinalReviewComponent = ({ onBack }) => {
-    const [referTransfer, setReferTransfer] = useState(false);
+const ReferralTransferView = ({onBack}) => {
+     const [referTransfer, setReferTransfer] = useState(false);
+  return (
+    <>
+        <div className="card">
+        <div className="cardLight mb-30">
+          <div className='btnBackTop'>
+            <button type="button" class="btnBack" onClick={onBack}></button>
+            <h2><AutoTranslate>Case & Evidence Information</AutoTranslate></h2>
+          </div>
 
-    return (
-        <>
-            <div className="card">
+          <div className="grid grid-col-4 mb-4">
+            <div className="form-group">
+              <label><AutoTranslate>Case Number</AutoTranslate></label>
+              <input type="text" placeholder="" name="" value="SFSL/145/2026" readOnly />
+            </div>
+            <div className="form-group">
+              <label><AutoTranslate>FIR Number</AutoTranslate></label>
+              <input type="text" placeholder="" name="" value="145/2026" readOnly />
+            </div>
+            <div className="form-group">
+              <label><AutoTranslate>Police Station</AutoTranslate></label>
+              <input type="text" placeholder="" name="" value="Saheed Nagar" readOnly />
+            </div>
+            <div className="form-group">
+              <label><AutoTranslate>District</AutoTranslate></label>
+              <input type="text" placeholder="" name="" value="Khordha" readOnly />
+            </div>
+            <div className="form-group">
+              <label><AutoTranslate>Incident Date</AutoTranslate></label>
+              <input type="text" placeholder="" name="" value="15-Aug-2026" readOnly />
+            </div>
+            <div className="form-group">
+              <label><AutoTranslate>Forwarding Authority</AutoTranslate></label>
+              <input type="text" placeholder="" name="" value="Saheed Nagar Police Station" readOnly />
+            </div>
+
+          </div>
+        </div>
+
+
+
+        <div class="grid grid-col-4 mb-4">
+          <div class="form-group"><label for="itemsPerPage"><span class="">Show:</span></label><select id="itemsPerPage">
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+          </select></div>
+          <div class="form-group"><label for="searchId"><span class="">Search</span></label>
+            <input type="text" id="searchId" placeholder="Search..." class="searchIcon" value="" />
+          </div>
+        </div>
+
+
+        <div class="table-wrapper">
+          <table class="">
+            <thead>
+              <tr>
+                <th><AutoTranslate>Evidence ID</AutoTranslate></th>
+                <th><AutoTranslate>Evidence Category</AutoTranslate></th>
+                <th><AutoTranslate>Evidence Type</AutoTranslate></th>
+                <th><AutoTranslate>Division</AutoTranslate></th>
+                <th><AutoTranslate>Scientific Officer</AutoTranslate></th>
+                <th><AutoTranslate>Report Status</AutoTranslate></th>
+                <th><AutoTranslate>Referral Status</AutoTranslate></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><AutoTranslate>EVD-001</AutoTranslate></td>
+                <td><AutoTranslate>EVD-001</AutoTranslate></td>
+                <td><AutoTranslate>Biological Sample</AutoTranslate></td>
+                <td><AutoTranslate>DNA</AutoTranslate></td>
+                <td><AutoTranslate>Officer A</AutoTranslate></td>
+                <td><AutoTranslate>Submitted</AutoTranslate></td>
+                <td><AutoTranslate>--</AutoTranslate></td>
+              </tr>
+              <tr>
+                <td><AutoTranslate>EVD-002</AutoTranslate></td>
+                <td><AutoTranslate>EVD-002</AutoTranslate></td>
+                <td><AutoTranslate>CCTV Footage</AutoTranslate></td>
+                <td><AutoTranslate>Cyber</AutoTranslate></td>
+                <td><AutoTranslate>Officer B</AutoTranslate></td>
+                <td><AutoTranslate>Submitted</AutoTranslate></td>
+                <td><AutoTranslate>--</AutoTranslate></td>
+              </tr>
+              <tr>
+                <td><AutoTranslate>EVD-003</AutoTranslate></td>
+                <td><AutoTranslate>EVD-003</AutoTranslate></td>
+                <td><AutoTranslate>Firearm</AutoTranslate></td>
+                <td><AutoTranslate>Ballistics</AutoTranslate></td>
+                <td><AutoTranslate>Officer c</AutoTranslate></td>
+                <td><AutoTranslate>Submitted</AutoTranslate></td>
+                <td><AutoTranslate>--</AutoTranslate></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        {/* Pagination Controls */}
+        <div class="paginationWp mb-20">
+          <div class="items">
+            <div class="paginationText">
+              <span class="text-sm text-gray-700">
+                <span class="">Showing 0 to 0 of 0 entries.</span></span>
+              <span class="text-sm text-gray-700 mx-2">(<span class="">Pages</span> 0)</span>
+            </div>
+          </div>
+          <div class="items">
+            <div class="paginationBtn"><button title="End" disabled="" class="cursor-not-allowed"><svg
+              stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em"
+              width="1em" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M217.9 256L345 129c9.4-9.4 9.4-24.6 0-33.9-9.4-9.4-24.6-9.3-34 0L167 239c-9.1 9.1-9.3 23.7-.7 33.1L310.9 417c4.7 4.7 10.9 7 17 7s12.3-2.3 17-7c9.4-9.4 9.4-24.6 0-33.9L217.9 256z">
+              </path>
+            </svg></button><button title="End" disabled="" class="cursor-not-allowed"><svg stroke="currentColor"
+              fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z">
+              </path>
+            </svg></button></div>
+          </div>
+        </div>
+     
+
+      
                 <div className="cardLight">
-                    <div className='btnBackTop'>
-                        <button type="button" class="btnBack" onClick={onBack}></button>
+                    <div className='btnBackTop'>                        
                         <h2><AutoTranslate>Scientific Examination</AutoTranslate></h2>
                     </div>
 
@@ -121,11 +241,7 @@ const FinalReviewComponent = ({ onBack }) => {
                     </div>
 
                 </div>
-
-                <div class="btn-group">
-                    <button type="button" class="btn-primary">Save</button>
-                    <button type="button" class="btn-primary">Return</button>
-                    <button type="button" class="btn-primary">Approve</button>
+                <div class="btn-group">    
                     <button type="button" class="btn-primary" onClick={() => { setReferTransfer(true) }}>Refer / Transfer</button>
                     <button type="button" class="btn btn-back" onClick={onBack}>Back</button>
                 </div>
@@ -215,8 +331,9 @@ const FinalReviewComponent = ({ onBack }) => {
                     </div>
                 </div>
             )}
-        </>
-    )
+    </>
+  )
 }
 
-export default FinalReviewComponent;
+export default ReferralTransferView;
+
